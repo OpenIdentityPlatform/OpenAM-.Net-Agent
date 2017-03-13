caafe621ba4a8f321dbee477d4a0e7c9a2f21de6
vharseko <vharseko@openam.org.ru>
2017-03-13 18:43:44 +0300

    2017-03-11 02:34:25.0530 Info (web request id: 4cc1784d-bb11-440e-b0f2-86fc95b4ce06) POST https://login.mts.ru/amserver/namingservice Content-Type: text/xml; encoding='utf-8' User-Agent: openam.org.ru (.Net 1.0.63/master-8dd43c4 money.mts.ru EKSITEWEB01/10.72.229.1) Host: login.mts.ru Cookie: track=6d3e37ef-6133-4f07-af7a-2e420d757a7c Content-Length: 322 Connection: Keep-Alive
    <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
    <RequestSet vers="1.0" svcid="com.iplanet.am.naming" reqid="1">
      <Request><![CDATA[<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
    <NamingRequest vers="3.0" reqid="1">
      <GetNamingProfile></GetNamingProfile>
    </NamingRequest>]]></Request>
    </RequestSet>
    
    2017-03-11 02:34:25.0999 Fatal (web request id: 4cc1784d-bb11-440e-b0f2-86fc95b4ce06) System.TypeInitializationException: The type initializer for 'ru.org.openam.sdk.Bootstrap' threw an exception. ---> System.Net.WebException: The remote server returned an error: (502) Bad Gateway.
       at System.Net.HttpWebRequest.GetResponse()
       at ru.org.openam.sdk.pll.Request.getResponse()
       at ru.org.openam.sdk.Naming.Get(Request request)
       at ru.org.openam.sdk.Bootstrap..cctor()
       --- End of inner exception stack trace ---
       at ru.org.openam.sdk.pll.Request.GetNaming()
       at ru.org.openam.sdk.auth.Request.getUrl()
       at ru.org.openam.sdk.pll.Request.getHttpWebRequest()
       at ru.org.openam.sdk.pll.Request.getResponse()
       at ru.org.openam.sdk.Auth.login(String realm, indexType indexType, String IndexName, Callback[] callbacks)
       at ru.org.openam.sdk.Agent.getSession()
       at ru.org.openam.sdk.Agent.GetConfig()
       at ru.org.openam.sdk.Agent.GetSingle(String name)
       at ru.org.openam.iis.OpenAMHttpModule.GetUserIp(HttpRequestBase request)
       at ru.org.openam.iis.OpenAMHttpModule.OnBeginRequest(HttpContextBase context)
    2017-03-11 02:34:25.3499 Fatal (web request id: d221df10-d6e0-4744-bce6-20bd07218494) System.TypeInitializationException: The type initializer for 'ru.org.openam.sdk.Bootstrap' threw an exception. ---> System.Net.WebException: The remote server returned an error: (502) Bad Gateway.
       at System.Net.HttpWebRequest.GetResponse()
       at ru.org.openam.sdk.pll.Request.getResponse()
       at ru.org.openam.sdk.Naming.Get(Request request)
       at ru.org.openam.sdk.Bootstrap..cctor()
       --- End of inner exception stack trace ---
       at ru.org.openam.sdk.pll.Request.GetNaming()
       at ru.org.openam.sdk.auth.Request.getUrl()
       at ru.org.openam.sdk.pll.Request.getHttpWebRequest()
       at ru.org.openam.sdk.pll.Request.getResponse()
       at ru.org.openam.sdk.Auth.login(String realm, indexType indexType, String IndexName, Callback[] callbacks)
       at ru.org.openam.sdk.Agent.getSession()
       at ru.org.openam.sdk.Agent.GetConfig()
       at ru.org.openam.sdk.Agent.GetSingle(String name)
       at ru.org.openam.iis.OpenAMHttpModule.GetUserIp(HttpRequestBase request)
       at ru.org.openam.iis.OpenAMHttpModule.OnBeginRequest(HttpContextBase context)

--- ru.org.openam.sdk/Bootstrap.cs
+++ ru.org.openam.sdk/Bootstrap.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net;
+using System.Threading;
 
 namespace ru.org.openam.sdk
 {
@@ -35,40 +36,51 @@ namespace ru.org.openam.sdk
             	return ConfigurationManager.AppSettings["com.iplanet.am.service.password"];
         }
 
-        //<Attribute name="iplanet-am-naming-session-class" value="com.iplanet.dpro.session.service.SessionRequestHandler"></Attribute>
-        //<Attribute name="iplanet-am-naming-samlsoapreceiver-url" value="%protocol://%host:%port%uri/SAMLSOAPReceiver"></Attribute>
-        //<Attribute name="02" value="http://login.staging.rapidsoft.ru:80/auth"></Attribute>
-        //<Attribute name="iplanet-am-naming-auth-url" value="%protocol://%host:%port%uri/authservice"></Attribute>
-        //<Attribute name="sun-naming-idsvcs-rest-url" value="%protocol://%host:%port%uri/identity/"></Attribute>
-        //<Attribute name="04" value="https://login.staging.rapidsoft.ru:444/auth"></Attribute>
-        //<Attribute name="iplanet-am-platform-site-id-list" value="04,05,01|02|05|06|04,06,02,03"></Attribute>
-        //<Attribute name="iplanet-am-naming-fsassertionmanager-url" value="%protocol://%host:%port%uri/FSAssertionManagerServlet/FSAssertionManagerIF"></Attribute>
-        //<Attribute name="openam-am-platform-site-names-list" value="dmz|02"></Attribute>
-        //<Attribute name="iplanet-am-naming-auth-class" value="com.sun.identity.authentication.server.AuthXMLHandler"></Attribute>
-        //<Attribute name="06" value="http://test.rapidsoft.ru:8080/auth"></Attribute>
-        //<Attribute name="iplanet-am-naming-samlawareservlet-url" value="%protocol://%host:%port%uri/SAMLAwareServlet"></Attribute>
-        //<Attribute name="iplanet-am-platform-lb-cookie-value-list" value="01|01,03|03"></Attribute>
-        //<Attribute name="serviceObjectClasses" value="iplanet-am-naming-service"></Attribute>
-        //<Attribute name="iplanet-am-platform-server-list" value="https://login.staging.rapidsoft.ru:444/auth,http://sso.rapidsoft.ru:8080/auth,http://localhost.rapidsoft.ru:8080/auth,http://test.rapidsoft.ru:8080/auth,http://login.staging.rapidsoft.ru:80/auth"></Attribute>
-        //<Attribute name="iplanet-am-naming-samlassertionmanager-url" value="%protocol://%host:%port%uri/AssertionManagerServlet/AssertionManagerIF"></Attribute>
-        //<Attribute name="03" value="http://localhost.rapidsoft.ru:8080/auth"></Attribute>
-        //<Attribute name="sun-naming-idsvcs-jaxws-url" value="%protocol://%host:%port%uri/identityservices/"></Attribute>
-        //<Attribute name="iplanet-am-naming-policy-class" value="com.sun.identity.policy.remote.PolicyRequestHandler"></Attribute>
-        //<Attribute name="sun-naming-sts-mex-url" value="%protocol://%host:%port%uri/sts/mex"></Attribute>
-        //<Attribute name="iplanet-am-naming-profile-url" value="%protocol://%host:%port%uri/profileservice"></Attribute>
-        //<Attribute name="iplanet-am-naming-session-url" value="%protocol://%host:%port%uri/sessionservice"></Attribute>
-        //<Attribute name="sun-naming-sts-url" value="%protocol://%host:%port%uri/sts"></Attribute>
-        //<Attribute name="iplanet-am-naming-logging-url" value="%protocol://%host:%port%uri/loggingservice"></Attribute>
-        //<Attribute name="iplanet-am-naming-securitytokenmanager-url" value="%protocol://%host:%port%uri/SecurityTokenManagerServlet/SecurityTokenManagerIF"></Attribute>
-        //<Attribute name="iplanet-am-naming-samlpostservlet-url" value="%protocol://%host:%port%uri/SAMLPOSTProfileServlet"></Attribute>
-        //<Attribute name="iplanet-am-naming-jaxrpc-url" value="%protocol://%host:%port%uri/jaxrpc/"></Attribute>
-        //<Attribute name="iplanet-am-naming-profile-class" value="com.iplanet.dpro.profile.agent.ProfileService"></Attribute>
-        //<Attribute name="iplanet-am-naming-logging-class" value="com.sun.identity.log.service.LogService"></Attribute>
-        //<Attribute name="iplanet-am-naming-policy-url" value="%protocol://%host:%port%uri/policyservice"></Attribute>
-        //<Attribute name="05" value="http://sso.rapidsoft.ru:8080/auth"></Attribute>
-        static naming.Response globalNaming = Naming.Get(new naming.Request());
+		//<Attribute name="iplanet-am-naming-session-class" value="com.iplanet.dpro.session.service.SessionRequestHandler"></Attribute>
+		//<Attribute name="iplanet-am-naming-samlsoapreceiver-url" value="%protocol://%host:%port%uri/SAMLSOAPReceiver"></Attribute>
+		//<Attribute name="02" value="http://login.staging.rapidsoft.ru:80/auth"></Attribute>
+		//<Attribute name="iplanet-am-naming-auth-url" value="%protocol://%host:%port%uri/authservice"></Attribute>
+		//<Attribute name="sun-naming-idsvcs-rest-url" value="%protocol://%host:%port%uri/identity/"></Attribute>
+		//<Attribute name="04" value="https://login.staging.rapidsoft.ru:444/auth"></Attribute>
+		//<Attribute name="iplanet-am-platform-site-id-list" value="04,05,01|02|05|06|04,06,02,03"></Attribute>
+		//<Attribute name="iplanet-am-naming-fsassertionmanager-url" value="%protocol://%host:%port%uri/FSAssertionManagerServlet/FSAssertionManagerIF"></Attribute>
+		//<Attribute name="openam-am-platform-site-names-list" value="dmz|02"></Attribute>
+		//<Attribute name="iplanet-am-naming-auth-class" value="com.sun.identity.authentication.server.AuthXMLHandler"></Attribute>
+		//<Attribute name="06" value="http://test.rapidsoft.ru:8080/auth"></Attribute>
+		//<Attribute name="iplanet-am-naming-samlawareservlet-url" value="%protocol://%host:%port%uri/SAMLAwareServlet"></Attribute>
+		//<Attribute name="iplanet-am-platform-lb-cookie-value-list" value="01|01,03|03"></Attribute>
+		//<Attribute name="serviceObjectClasses" value="iplanet-am-naming-service"></Attribute>
+		//<Attribute name="iplanet-am-platform-server-list" value="https://login.staging.rapidsoft.ru:444/auth,http://sso.rapidsoft.ru:8080/auth,http://localhost.rapidsoft.ru:8080/auth,http://test.rapidsoft.ru:8080/auth,http://login.staging.rapidsoft.ru:80/auth"></Attribute>
+		//<Attribute name="iplanet-am-naming-samlassertionmanager-url" value="%protocol://%host:%port%uri/AssertionManagerServlet/AssertionManagerIF"></Attribute>
+		//<Attribute name="03" value="http://localhost.rapidsoft.ru:8080/auth"></Attribute>
+		//<Attribute name="sun-naming-idsvcs-jaxws-url" value="%protocol://%host:%port%uri/identityservices/"></Attribute>
+		//<Attribute name="iplanet-am-naming-policy-class" value="com.sun.identity.policy.remote.PolicyRequestHandler"></Attribute>
+		//<Attribute name="sun-naming-sts-mex-url" value="%protocol://%host:%port%uri/sts/mex"></Attribute>
+		//<Attribute name="iplanet-am-naming-profile-url" value="%protocol://%host:%port%uri/profileservice"></Attribute>
+		//<Attribute name="iplanet-am-naming-session-url" value="%protocol://%host:%port%uri/sessionservice"></Attribute>
+		//<Attribute name="sun-naming-sts-url" value="%protocol://%host:%port%uri/sts"></Attribute>
+		//<Attribute name="iplanet-am-naming-logging-url" value="%protocol://%host:%port%uri/loggingservice"></Attribute>
+		//<Attribute name="iplanet-am-naming-securitytokenmanager-url" value="%protocol://%host:%port%uri/SecurityTokenManagerServlet/SecurityTokenManagerIF"></Attribute>
+		//<Attribute name="iplanet-am-naming-samlpostservlet-url" value="%protocol://%host:%port%uri/SAMLPOSTProfileServlet"></Attribute>
+		//<Attribute name="iplanet-am-naming-jaxrpc-url" value="%protocol://%host:%port%uri/jaxrpc/"></Attribute>
+		//<Attribute name="iplanet-am-naming-profile-class" value="com.iplanet.dpro.profile.agent.ProfileService"></Attribute>
+		//<Attribute name="iplanet-am-naming-logging-class" value="com.sun.identity.log.service.LogService"></Attribute>
+		//<Attribute name="iplanet-am-naming-policy-url" value="%protocol://%host:%port%uri/policyservice"></Attribute>
+		//<Attribute name="05" value="http://sso.rapidsoft.ru:8080/auth"></Attribute>
+
+		static String lockKey="";
+		static naming.Response globalNaming = null; //Naming.Get(new naming.Request());
         public static naming.Response GetNaming()
         {
+			if (globalNaming==null)
+				lock (lockKey) {
+					while (globalNaming == null) {
+						try {
+							globalNaming = Naming.Get (new naming.Request ());
+							Thread.Sleep (100);
+						} catch (Exception) { }
+					}
+				}
 			return globalNaming;
         }
 
