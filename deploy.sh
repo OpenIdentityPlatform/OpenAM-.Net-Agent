ba49579f900d11837749f398acfd86e28960a8ed
vharseko <vharseko@openam.org.ru>
2017-06-22 12:06:58 +0300

    fix msbuild+nuinit

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 rm -rf ru.org.openam.iis.httpmodule/bin
-xbuild /p:Configuration=Release ru.org.openam.dotnet.sln
-nunit-console ru.org.openam.nunit/bin/Release/ru.org.openam.sdk.nunit.dll
+msbuild /p:Configuration=Release ru.org.openam.dotnet.sln
+nuget install NUnit.Runners -Version 3.6.0 -OutputDirectory testrunner
+#nunit-console ru.org.openam.nunit/bin/Release/ru.org.openam.sdk.nunit.dll
+mono ./testrunner/NUnit.ConsoleRunner.3.6.0/tools/nunit3-console.exe ./ru.org.openam.nunit/bin/Release/ru.org.openam.sdk.nunit.dll
 rm -rf /tmp/ru.org.openam.iis.httpmodule.zip
 cp ru.org.openam.iis.httpmodule/Properties/AssemblyInfo.cs ru.org.openam.iis.httpmodule/bin/Release/ru.org.openam.version.txt
 zip -rjv  /tmp/ru.org.openam.iis.httpmodule.zip ru.org.openam.iis.httpmodule/bin/Release/*
-echo http://repo.openam.org.ru/ru.org.openam.iis.httpmodule.zip
 scp -P 23 /tmp/ru.org.openam.iis.httpmodule.zip repo.openam.org.ru:/var/www/repo
+cat ru.org.openam.iis.httpmodule/bin/Release/ru.org.openam.version.txt
+echo http://repo.openam.org.ru/ru.org.openam.iis.httpmodule.zip
+
